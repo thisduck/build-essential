dbd3275515b8d8d06ba3c2bca528eb0af3fe086b
Adnan Ali <adnan.ali@gmail.com>
2018-01-01 23:07:06 -0500

    changed head to tail.
    Since in High Sierra, the last line in the grep output is the
    command line tools we want to install.

--- resources/xcode_command_line_tools.rb
+++ resources/xcode_command_line_tools.rb
@@ -32,7 +32,7 @@ action :install do
           # in Apple's SUS catalog
           touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
           # find the CLI Tools update
-          PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+          PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
           # install it
           softwareupdate -i "$PROD" --verbose
           # Remove the placeholder to prevent perpetual appearance in the update utility
